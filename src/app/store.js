3c759c0b4208fba2ce15ff5e289a1532e5b38a32
Andy Kotz <andy.lyme@mac.com>
2023-11-03 16:27:56 -0400

    added auth

--- src/app/store.js
+++ src/app/store.js
@@ -1,11 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
 import surveyReducer from '../features/survey/surveySlice';
 import userGoalsReducer from '../features/usergoals/userGoalsSlice';
+import userReducer from '../features/user/userSlice';
 
 const store = configureStore({
   reducer: {
     survey: surveyReducer,
     userGoals: userGoalsReducer,
+    user: userReducer,
   },
 });
 
