48fd9f4e6756c79350d0b5fe268a5548cdf863cb
Andy Kotz <andy.lyme@mac.com>
2024-01-18 10:16:18 -0500

    added requests

--- src/features/usergoals/SustainabilityGoalInput.jsx
+++ src/features/usergoals/SustainabilityGoalInput.jsx
@@ -1,10 +1,14 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import { setGoal } from './userGoalsSlice';
+import { fetchGoals } from './userGoalsRequests';
 
 function SustyGoalInput() {
   const [goal, setGoalState] = useState('');
   const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(fetchGoals());
+  }, []);
   return (
     <div className="container">
       <h3>What is your sustainability goal?</h3>
--- src/features/usergoals/userGoalsRequests.js
+++ src/features/usergoals/userGoalsRequests.js
@@ -10,3 +10,24 @@ export function setGoalRequest(goal, actions) {
     dispatch(actions.setGoalReducer(response.data));
   };
 }
+
+export function fetchGoals(actions) {
+  return async (dispatch) => {
+    const response = await axios.get(`${ROOT_URL}/goals`, getAuthHeader());
+    dispatch(actions.setGoalReducer(response.data));
+  };
+}
+
+export function completeGoalRequest(goal, actions) {
+  return async (dispatch) => {
+    const response = await axios.post(`${ROOT_URL}/goals/complete`, goal, getAuthHeader());
+    dispatch(actions.setGoalReducer(response.data));
+  };
+}
+
+export function deleteGoalRequest(goal, actions) {
+  return async (dispatch) => {
+    const response = await axios.delete(`${ROOT_URL}/goals`, goal, getAuthHeader());
+    dispatch(actions.setGoalReducer(response.data));
+  };
+}
