e142145cf6579537dedaaa5e35635184687ca221
Andy Kotz <andy.lyme@mac.com>
2024-01-20 17:56:11 -0500

    fixed backend routing

--- src/features/usergoals/userGoalsRequests.js
+++ src/features/usergoals/userGoalsRequests.js
@@ -3,7 +3,6 @@ import { getAuthHeader } from '../../app/utils';
 
 const ROOT_URL = 'https://fsp-carbon-offsets-backend-lakm.onrender.com/api';
 
-// eslint-disable-next-line import/prefer-default-export
 export function setGoalRequest(goal, actions) {
   return async (dispatch) => {
     const response = await axios.post(`${ROOT_URL}/goals`, goal, getAuthHeader());
@@ -20,14 +19,14 @@ export function fetchGoals(actions) {
 
 export function completeGoalRequest(id, actions) {
   return async (dispatch) => {
-    const response = await axios.post(`${ROOT_URL}/goals/complete/${id}`, getAuthHeader());
+    const response = await axios.post(`${ROOT_URL}/goals/complete/${id}`, {}, getAuthHeader());
     dispatch(actions.setGoalReducer(response.data));
   };
 }
 
 export function failGoalRequest(id, actions) {
   return async (dispatch) => {
-    const response = await axios.post(`${ROOT_URL}/goals/fail/${id}`, getAuthHeader());
+    const response = await axios.post(`${ROOT_URL}/goals/fail/${id}`, {}, getAuthHeader());
     dispatch(actions.setGoalReducer(response.data));
   };
 }
