31100f4dbda4d387bf019b632fd09d5b4b624b9a
chloenicolaou <68208228+chloenicolaou@users.noreply.github.com>
2024-01-31 17:18:54 -0500

    push push

--- src/features/carbontracking/CarbonTracking.jsx
+++ src/features/carbontracking/CarbonTracking.jsx
@@ -1,10 +1,13 @@
 /* eslint-disable react/no-array-index-key */
 import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { Stack } from '@mui/joy';
+import {
+  Stack, Card, Typography, Box,
+} from '@mui/joy';
 import { fetchStops } from './carbonSlice';
 import FlightInput from './FlightInput';
 import CarbonFootprint from './CarbonFootprint';
+import logo from '../../img/climatiqWhite.png';
 
 function CarbonTracking(props) {
   const dispatch = useDispatch();
@@ -19,6 +22,21 @@ function CarbonTracking(props) {
     <Stack direction="column" justifyContent="flex-start" alignItems="stretch" spacing={2}>
       <FlightInput stops={stops} />
       <CarbonFootprint />
+      <Card variant="plain"
+        style={{
+          position: 'absolute', bottom: 0, left: 0, right: 0,
+        }}
+      >
+        <Stack direction="row"
+          alignItems="center"
+          justifyContent="flex-end"
+        >
+          <Typography level="h4" component="h1" sx={{ fontWeight: 'md' }}>
+            Travel Calculator Powered By
+          </Typography>
+          <Box component="img" src={logo} alt="Logo" sx={{ width: '8%', height: 'auto', padding: '16px' }} />
+        </Stack>
+      </Card>
     </Stack>
   );
 }
