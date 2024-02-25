c3b7cd9be7cc2f751a2775a4fadfc496bd26853b
Andy Kotz <andy.lyme@mac.com>
2024-02-25 14:54:11 -0500

    Update DashBoard.jsx

--- src/features/dashboard/DashBoard.jsx
+++ src/features/dashboard/DashBoard.jsx
@@ -1,29 +1,29 @@
 /* eslint-disable no-nested-ternary */
-import React, { useState } from 'react';
-import { useSelector } from 'react-redux';
+import React, { useState, useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import {
   Typography, Card, Stack, ToggleButtonGroup, Button,
 } from '@mui/joy';
 import CurrentGoals from '../usergoals/currentGoals';
 import TotalCarbonDonut from './totalCarbonDonut';
+import { fetchCarbonFootprint } from '../carbontracking/carbonSlice';
 
-function CarbonPieChart() {
-  const [pieType, setPieType] = useState('weekly');
+function CarbonPieChart({ footprint }) {
+  const [pieType, setPieType] = useState({ type: 'user', time: 'weekly' });
   const handleChange = (event) => {
-    setPieType(event.target.value);
+    const [user, frequency] = event.target.value.split(' ');
+    setPieType({ type: user, time: frequency });
   };
-  const footprint = useSelector((state) => state.carbon.footprint);
-  // const loading = footprint === 'loading' || footprint === 'undefined';
+
+  console.log(pieType);
 
   return (
     <div style={{ position: 'relative', marginTop: '20px' }}>
       <ToggleButtonGroup
-        value={pieType}
-        exclusive
+        value={`${pieType.type} ${pieType.time}`}
         onChange={handleChange}
         aria-label="Platform"
         position="sticky"
-        zIndex="999"
         color="primary"
       >
         <Button variant="outlined"
@@ -31,36 +31,41 @@ function CarbonPieChart() {
             justifyContent: 'start',
             '&:hover': { backgroundColor: 'rgba(195, 191, 191, 0.7)' },
             color: 'black',
-            borderColor: 'black', // Change outline color to black
+            borderColor: 'black',
           }}
-          value="weekly"
+          value="user weekly"
         >WEEKLY
         </Button>
         <Button sx={{
           justifyContent: 'start',
           '&:hover': { backgroundColor: 'rgba(195, 191, 191, 0.7)' },
           color: 'black',
-          borderColor: 'black', // Change outline color to black
+          borderColor: 'black',
         }}
-          value="allTime"
+          value="user allTime"
         >ALL TIME
         </Button>
         <Button sx={{
           justifyContent: 'start',
           '&:hover': { backgroundColor: 'rgba(195, 191, 191, 0.7)' },
           color: 'black',
-          borderColor: 'black', // Change outline color to black
+          borderColor: 'black',
         }}
-          value="team"
-        >TEAM
+          value="team weekly"
+        >TEAM WEEKLY
+        </Button>
+        <Button sx={{
+          justifyContent: 'start',
+          '&:hover': { backgroundColor: 'rgba(195, 191, 191, 0.7)' },
+          color: 'black',
+          borderColor: 'black',
+        }}
+          value="team allTime"
+        >TEAM ALL TIME
         </Button>
       </ToggleButtonGroup>
-      <div style={{ marginTop: '20px' }}> {/* Add marginTop */}
-        {pieType && pieType === 'weekly'
-          ? <TotalCarbonDonut points={footprint.weekly} />
-          : pieType === 'allTime'
-            ? <TotalCarbonDonut points={footprint.allTime} />
-            : <TotalCarbonDonut points={footprint.team} />}
+      <div style={{ marginTop: '20px' }}>
+        {pieType && <TotalCarbonDonut points={footprint[pieType.user][pieType.time]} />}
       </div>
     </div>
   );
@@ -69,13 +74,33 @@ function CarbonPieChart() {
 export { CarbonPieChart };
 
 function DashBoard() {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(fetchCarbonFootprint());
+  }, []);
+  const footprint = useSelector((state) => state.carbon.footprint);
+  console.log(footprint);
+
+  if (!footprint || footprint === 'loading') {
+    return (
+      <Card>
+        <Typography level="h3" component="h1" sx={{ fontWeight: 'md' }}>
+          Your Carbon Snapshot
+        </Typography>
+        <Typography level="h3" component="h1" sx={{ fontWeight: 'md' }}>
+          Loading...
+        </Typography>
+      </Card>
+    );
+  }
+
   return (
     <Stack>
       <Card variant="plain" sx={{ backgroundColor: 'transparent' }}>
         <Typography level="h3" component="h1" sx={{ fontWeight: 'md' }}>
           Your Carbon Snapshot
         </Typography>
-        {/* <CarbonPieChart /> */}
+        {(footprint && footprint !== 'loading') && <CarbonPieChart footprint={footprint} />}
         {/* <CarbonVis /> */}
       </Card>
       <Card variant="plain" sx={{ backgroundColor: 'transparent' }}>
